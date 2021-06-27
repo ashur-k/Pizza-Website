dae01b0062ccfe319905fcf8c5f9d5fcd7bbb04a
ashur-k <ashurkanwal@yahoo.com>
2021-06-27 19:53:40 +0100

    setup to fetch data from Django backened

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -4,63 +4,8 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 
-export default function Home() {
-  const pizzas = [
-    {
-      id: '1',
-      name: 'Cheese Pizza',
-      slug: 'cheese-pizza',
-      toppings: ['mozzrella Chees'],
-      image:'https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-      price: 9.99,
-      description: "really nice pizza"
-    },
-    {
-      id: '2',
-      name: 'Meat Feast',
-      slug: 'meet-feast',
-      toppings: ['Ham', 'Bacon', 'Pepporoni', 'Sussage'],
-      image:'https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80',
-      price: 9.99,
-      description: "really nice pizza"
-    },
-    {
-      id: '3',
-      name: 'Pepperoni Pizza',
-      slug: 'pepperoni-pizza',
-      toppings: ['mozzrella Chees'],
-      image:'https://images.unsplash.com/photo-1564128442383-9201fcc740eb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1102&q=80',
-      price: 9.99,
-      description: "really nice pizza"
-    },
-    {
-      id: '4',
-      name: 'Smoked Sussage Pizza',
-      slug: 'smoked-sussage-pizza',
-      toppings: ['mozzrella Chees'],
-      image:'https://images.unsplash.com/photo-1474600056930-615c3d706456?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
-      price: 9.99,
-      description: "really nice pizza"
-    },
-    {
-      id: '5',
-      name: 'Vege Pizza',
-      slug: 'vege-pizza',
-      toppings: ['mozzrella Chees'],
-      image:'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=714&q=80',
-      price: 9.99,
-      description: "really nice pizza"
-    },
-    {
-      id: '6',
-      name: 'Cheese Pizza',
-      slug: 'Chees-pizza',
-      toppings: ['mozzrella Chees'],
-      image:'https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1055&q=80',
-      price: 9.99,
-      description: "really nice pizza"
-    },
-  ]
+export default function Home({ pizzas }) {
+ 
 
   const [keyword, setKeyword] = useState('');
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import Layout from '../components/Layout/Layout';
 import Home from '../components/Home/Home'
 
-export default function HomePage() {
+export default function HomePage({ pizzas }) {
   return (
     <Layout>
-      <Home />
+      <Home pizzas={pizzas}/>
     </Layout>
   )
+}
+
+
+export const getStaticProps = async ({params}) => {
+
+  const res = await fetch('http://127.0.0.1:8000/api/pizzas/');
+  const pizzas = await res.json();
+
+
+  return {
+    props: {
+      pizzas,
+    }
+  }
+
+
 }
