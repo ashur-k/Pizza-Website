2a98ad7ce229daf1fbbfafbfbdfd344bea756da9
ashur-k <ashurkanwal@yahoo.com>
2021-06-28 10:45:03 +0100

    updated footer page

--- pages/[slug].js
+++ pages/[slug].js
@@ -48,7 +48,9 @@ export default function Pizza({pizza, otherPizzas}) {
     </div>
     <footer className={styles.footer}>
       <a>
-      Practicing NEXT-JS Django CODING. Toutorial made by Cocoa.
+          Practicing NEXT-JS Django CODING. Toutorial made by Cocoa.
+          I coded Django API myself, Cocoa was using classbased views,
+          and I like using function based views. 
       </a>
     </footer>
   </div>
